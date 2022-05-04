423a7d0caca15e3e765c4004bc196607087da463
Christopher White <christopher.white@slicelife.com>
2022-05-04 12:46:47 +0100

    Added metrickit logging for initial attributes

--- Sources/metric-logging/metric_logging.swift
+++ Sources/metric-logging/metric_logging.swift
@@ -1,6 +1,101 @@
-public struct metric_logging {
-    public private(set) var text = "Hello, World!"
+import MetricKit
+import Combine
 
-    public init() {
+public class metric_logging: NSObject {
+    var metricAttributes: AnyPublisher<[String: Double], Never> { return _metricAttributes.eraseToAnyPublisher() }
+
+    private let _metricAttributes = PassthroughSubject<[String: Double], Never>()
+
+    func start() {
+        MXMetricManager.shared.add(self)
+    }
+}
+
+protocol HandlesSliceMetricAttributes {
+    func didReceiveMetricPayloads(_ payloads: [MXMetricPayload])
+}
+
+extension metric_logging: MXMetricManagerSubscriber {
+    public func didReceive(_ payloads: [MXMetricPayload]) {
+        guard let currentAppVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else {
+            return
+        }
+
+        var attributes: [String: Double] = [:]
+
+        if let averageTimeToFirstDraw = payloads.average(for: \.applicationLaunchMetrics, applicationVersion: currentAppVersion) {
+            attributes["first_draw_avg"] = averageTimeToFirstDraw.value
+        }
+        if let averageHangTime = payloads.average(for: \.applicationResponsivenessMetrics, applicationVersion: currentAppVersion) {
+            attributes["hang_time_avg"] = averageHangTime.value
+        }
+
+        guard !attributes.isEmpty else { return }
+
+        _metricAttributes.send(attributes)
+    }
+}
+
+protocol HistogrammedTimeMetric {
+    var histogram: MXHistogram<UnitDuration> { get }
+    var average: Measurement<UnitDuration> { get }
+}
+
+extension HistogrammedTimeMetric {
+    /// Calculates the average duration in milliseconds for the given histogram values.
+    var average: Measurement<UnitDuration> {
+        let buckets = histogram.bucketEnumerator.compactMap { $0 as? MXHistogramBucket }
+        let totalBucketsCount = buckets.reduce(0) { totalCount, bucket in
+            var totalCount = totalCount
+            totalCount += bucket.bucketCount
+            return totalCount
+        }
+        let totalDurations: Double = buckets.reduce(0) { totalDuration, bucket in
+            var totalDuration = totalDuration
+            totalDuration += Double(bucket.bucketCount) * bucket.bucketEnd.value
+            return totalDuration
+        }
+        let average = totalDurations / Double(totalBucketsCount)
+        return Measurement(value: average, unit: UnitDuration.milliseconds)
+    }
+}
+
+extension MXAppLaunchMetric: HistogrammedTimeMetric {
+    var histogram: MXHistogram<UnitDuration> {
+        histogrammedTimeToFirstDraw
+    }
+}
+
+extension MXAppResponsivenessMetric: HistogrammedTimeMetric {
+    var histogram: MXHistogram<UnitDuration> {
+        histogrammedApplicationHangTime
+    }
+}
+
+extension Array where Element == MXMetricPayload {
+
+    /// Calculates the average Metric value for all payloads containing the given key path for the given application version.
+    func average<Value: HistogrammedTimeMetric>(for keyPath: KeyPath<MXMetricPayload, Value?>, applicationVersion: String) -> Measurement<UnitDuration>? {
+        let averages = filter(for: applicationVersion)
+            .compactMap { payload in
+                payload[keyPath: keyPath]?.average.value
+            }
+
+        guard !averages.isEmpty else { return nil }
+
+        let average: Double = averages.reduce(0.0, +) / Double(averages.count)
+        guard !average.isNaN else { return nil }
+
+        return Measurement(value: average, unit: UnitDuration.milliseconds)
+    }
+
+    private func filter(for applicationVersion: String) -> [MXMetricPayload] {
+        filter { payload in
+            guard !payload.includesMultipleApplicationVersions else {
+                // We only want to use payloads for the latest app version
+                return false
+            }
+            return payload.latestApplicationVersion == applicationVersion
+        }
     }
 }
--- Tests/metric-loggingTests/metric_loggingTests.swift
+++ Tests/metric-loggingTests/metric_loggingTests.swift
@@ -6,6 +6,5 @@ final class metric_loggingTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(metric_logging().text, "Hello, World!")
     }
 }
